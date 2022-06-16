96b5d866ccc80f5e40b3f814a95909e39c0a3808
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2022-06-16 11:49:38 +0300

    remove webpack dependency

--- product-service/webpack.config.js
+++ product-service/webpack.config.js
@@ -7,14 +7,7 @@ module.exports = {
   target: 'node',
   mode: process.env.NODE_ENV,
   module: {
-    rules: [
-      // {
-      //   test: /\.js$/,
-      //   use: [
-      //     { loader: 'babel-loader' }
-      //   ]
-      // },
-    ],
+    rules: [],
   },
   output: {
     libraryTarget: 'commonjs',
