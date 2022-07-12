4b8036282d08ee7ec8a0f7981eeb37802bd7d9a8
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2022-07-12 10:10:46 +0300

    Fix processing of create product notifications

--- src/product-service/handlers/catalogBatchProcess.js
+++ src/product-service/handlers/catalogBatchProcess.js
@@ -1,5 +1,5 @@
 import aws from 'aws-sdk';
-import { createProducts } from '../services/index.js';
+import { createMultipleProducts, createProducts } from '../services/index.js';
 import { logError, logSuccess } from '../loggers/index.js';
 
 const { REGION, SNS_ARN } = process.env;
@@ -7,40 +7,53 @@ const { REGION, SNS_ARN } = process.env;
 // polls data from SQS and saves it to DB + SNS
 export default async (event, context) => {
   const sns = new aws.SNS({ region: REGION });
-  const products = event.Records.map(record => JSON.parse(record.body));
   try {
-    // TODO: replace with batch of products creation
-    await Promise.all(products.map(product => createProducts(product)));
-    const PublishBatchRequestEntries = products.map(({ title }) => ({
-      Id: title,
-      Subject: 'Product is created',
-      Message: `Product "${title}" is added to DB successfully`,
-      MessageAttributes: {
-        category: {
-          DataType: 'String',
-          StringValue: 'info',
-        },
+    const products = event.Records.map(record => JSON.parse(record.body));
+    const values = await Promise.allSettled(products.map(product => createProducts(product)));
+    for (let i = 0; i < products.length; ++i) {
+      const { status, reason } = values[i];
+      const product = products[i];
+      if (status === 'fulfilled') {
+        await sns.publish({
+          Subject: 'Product is created',
+          Message: `Product "${product.title}" is added to DB successfully`,
+          TopicArn: SNS_ARN,
+          MessageAttributes: {
+            category: {
+              DataType: 'String',
+              StringValue: 'info',
+            }
+          },
+        }).promise();
+      } else {
+        await sns.publish({
+          Subject: 'Product creation failed',
+          Message: `Product failed to be added to DB:\n${JSON.stringify(product, null, 4)}\nReason: ${reason}`,
+          TopicArn: SNS_ARN,
+          MessageAttributes: {
+            category: {
+              DataType: 'String',
+              StringValue: 'warn',
+            }
+          },
+        }).promise();
       }
-    }));
-    await sns.publishBatch({
-      PublishBatchRequestEntries,
-      TopicArn: SNS_ARN
-    }).promise();
-    logSuccess(event, context);
+    }
     return {
       statusCode: 200
     }
   } catch (error) {
+    const recordBodies = event.Records.map(record => record.body).join('\n');
     await sns.publish({
-      Subject: 'Product creation failed',
-      Message: `Products failed to be added to DB:\n${JSON.stringify(products, null, 4)}\n${error.message}`,
+      Subject: 'Error during creation of products',
+      Message: `Products failed to be added to DB. Records bodies:\n${recordBodies}\nReason: ${error.message}`,
       TopicArn: SNS_ARN,
       MessageAttributes: {
         category: {
           DataType: 'String',
           StringValue: 'error',
         }
-      }
+      },
     }).promise();
     logError(event, context, error.message);
     return {
